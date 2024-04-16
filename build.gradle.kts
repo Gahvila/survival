f53e20a9e0d9adab9687c8589d91abb47861e81b
DaJokni <56400974+DaJokni@users.noreply.github.com>
2024-04-16 23:15:26 +0300

    dependency päivityksiä

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,14 +14,15 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly ("me.clip:placeholderapi:2.11.5")
     compileOnly ("net.luckperms:api:5.4")
     compileOnly("de.hexaoxi:carbonchat-api:3.0.0-beta.26")
     compileOnly ("com.github.koca2000:NoteBlockAPI:1.6.2")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     implementation ("com.github.stefvanschie.inventoryframework:IF:0.10.14-SNAPSHOT")
-    implementation ("com.github.simplix-softworks:simplixstorage:3.2.7")
+    implementation ("com.github.DaJokni:simplixstorage:-SNAPSHOT")
+    compileOnly ("com.github.DaJokni:CrashClaim:-SNAPSHOT")
 
     //commandapi
     implementation("dev.jorel:commandapi-bukkit-shade:9.3.0")
